20349eba0f2ea7d38c80c7b641c02736a27a05ba
Clicker-games-Studio <clickergamesstudio@gmail.com>
2026-01-07 20:26:26 +0100

    Add psutil package installation to script
    Added installation of the 'psutil' package.

--- install_required.bat
+++ install_required.bat
@@ -3,7 +3,9 @@ echo Installing required Python packages...
 
 "python\python\python.exe" -m pip install --upgrade pip
 "python\python\python.exe" -m pip install msal
+"python\python\python.exe" -m pip install psutil
 
 echo.
 echo Installation complete!
 pause
+
